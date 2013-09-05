e18f5cf887bcfb15cc236cb5a0225a828dc8de0f
Andy Gill <andygill@ku.edu>
2013-09-05 15:14:58 -0500

    New Main function for LDPC stuff.

---
+++ src/Main.hs
@@ -0,0 +1,14 @@
+module Main where
+
+import ECC
+
+import qualified Manifold.Array as A
+
+pickECC :: String -> ECC
+pickECC "array-moon:200" = A.moon_array_ecc 200
+
+main = do
+        let ecc0 = pickECC "array-moon:200"
+        let ecc1 = txRx_EbN0 20.0 ecc0
+        runECC (Just 0) 100 ecc1 { verbose = 0 }
+
