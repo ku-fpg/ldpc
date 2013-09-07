f430f9b39b8da925778c37504895c84bac5e7cd1
Andy Gill <andygill@ku.edu>
2013-09-07 14:10:28 -0500

    Adding new decoder. Should this always go a in separate model?

--- src/Haskell/Array/Decode.hs
+++ src/Haskell/Array/Decode.hs
@@ -38,4 +38,36 @@ ldpc maxIterations a orig_lam = fmap hard $ loop 0 ne orig_lam
 
         lam' = accum (+) orig_lam [ (n,a) | ((_,n),a) <- assocs ne' ]
 
+decoder' :: Int -> M Bit -> V Double -> V Bit
+decoder' = ldpc'
 
+ldpc' :: (Floating d, Ord d) => Int -> M Bit -> V d -> V Bit
+ldpc' maxIterations a orig_lam = fmap hard $ loop 0 ne orig_lam
+  where
+    ne = fmap (const 0) a
+
+    loop !n ne lam
+        | null [ () | 1 <- elems ans ] = lam
+        | n >= maxIterations           = orig_lam
+        | otherwise                    = loop (n+1) ne' lam'
+      where
+        c_hat :: V Bit
+        c_hat = fmap hard lam
+
+        ans :: M Bit
+        ans = a `mm` columnM c_hat
+
+        ne' = ne // [ ((m,n), -0.75 * foldr1 min'
+                         [ - (lam ! j - ne ! (m,j))
+                         | j <- indices lam
+                         , j /= n
+                         , a ! (m,j) == 1
+                         ])
+                    | (m,n) <- indices ne
+                    , a ! (m,n) == 1
+                    ]
+
+        lam' = accum (+) orig_lam [ (n,a) | ((_,n),a) <- assocs ne' ]
+
+min' :: (Num a, Ord a) => a -> a -> a
+min' x y = signum x * signum y * min (abs x) (abs y)
